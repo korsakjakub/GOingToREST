d5bb2cb918f095fef64fb59e1515dc551381b483
Jakub Korsak <jakub.korsak@bluesoft.com>
2022-07-15 18:04:49 +0200

    Hangle GETs @8000 and return redis size

---
+++ explorer/explorer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	redis "github.com/go-redis/redis/v9"
+	mux "github.com/gorilla/mux"
+)
+
+var ctx = context.Background()
+var rdb = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "",
+	DB:       0,
+})
+
+func getSize(w http.ResponseWriter, r *http.Request) {
+
+	val, err := rdb.DBSize(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, strconv.FormatInt(val, 10))
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/size", getSize).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
