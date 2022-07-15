f87ec680182fbd010c63f9846ab0475805b1d876
Jakub Korsak <jakub.korsak@bluesoft.com>
2022-07-15 16:58:27 +0200

    run_rabbitmq - script to create rabbitmq container, post_curl - POSTs a random json of User form

---
+++ scripts/post_curl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+id="$(($RANDOM % 666))"
+age=$"$((10 + $RANDOM % 50))"
+name="$(echo $RANDOM | md5sum | head -c 10; echo;)"
+surname="$(echo $RANDOM | md5sum | head -c 10; echo;)"
+jsonfmt='{"id": "%s", "name":"%s", "surname":"%s","age":"%s"}'
+#json=$(printf "$jsonfmt" "$id" "$name" "$surname" "$age")
+json=$(jq -n --arg ii "$id" --arg nm "$name" --arg sr "$surname" '{id: $ii, name: $nm, surname: $sr}')
+curl -X POST -H "Content-Type: application-json" -d "$json" localhost:8000/add
