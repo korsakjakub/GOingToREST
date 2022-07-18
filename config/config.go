1d510aa6083832df50349c2724c5f3c9a6a97c57
Jakub Korsak <jakub.korsak@protonmail.com>
2022-07-18 15:23:39 +0200

    init commit for config

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type RedisConfig struct {
+	Address string `mapstructure:"address"`
+	Port 	string `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	db string `mapstructure:"db"`
+}
+
+type RabbitMQConfig struct {
+	Login string `mapstructure:"login"`
+	Password string `mapstructure:"password"`
+	Address string `mapstructure:"address"`
+	Port string `mapstructure:"port"`
+}
+
+type Config struct {
+	Redis RedisConfig `mapstructure:"redis"`
+	RabbitMQ RabbitMQConfig `mapstructure:"rabbitmq"`
+}
+
+var vp *viper.Viper
+
+func LoadConfig() (Config, error) {
+	vp = viper.New()
+	var config Config
+
+	vp.SetConfigName("config")
+	vp.SetConfigType("yaml")
+	vp.AddConfigPath("./config")
+	vp.AddConfigPath(".")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return Config{}, err
+	}
+
+	err = vp.Unmarshal(&config)
+	if err != nil {
+		return Config{}, err
+	}
+
+
+	return config, nil
+}
