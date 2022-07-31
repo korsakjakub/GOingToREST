c0226a20f788387e5d1420dd050e3fbd32fd20b8
Jakub Korsak <jakub.korsak@protonmail.com>
2022-07-31 17:15:45 +0200

    kubernetes now works

--- scripts/post_curl.sh
+++ scripts/post_curl.sh
@@ -6,5 +6,5 @@ surname="$(echo $RANDOM | md5sum | head -c 10; echo;)"
 jsonfmt='{"id": "%s", "name":"%s", "surname":"%s","age":"%s"}'
 #json=$(printf "$jsonfmt" "$id" "$name" "$surname" "$age")
 json=$(jq -n --arg ii "$id" --arg nm "$name" --arg sr "$surname" --arg ag "$age" '{id: $ii, name: $nm, surname: $sr, age: $ag}')
-curl -X POST -H "Content-Type: application-json" -d "$json" 20.105.72.86/add
+curl -X POST -H "Content-Type: application-json" -d "$json" $1
 #curl -X POST -H "Content-Type: application-json" -d "$json" localhost:6666/add
